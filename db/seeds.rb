d95b7c07db393dd62376861ce6a489a6652a043b
Fanny Rostaing <fanny@coffretis.com>
2020-08-12 16:01:32 +0200

    style one the way

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  flat = Flat.new(
+    name: Faker::Address.community,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(50...100),
+    number_of_guest: rand(1..10),
+    )
+  flat.save!
+end
