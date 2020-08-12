2b636bb8916f6e510f3bfe93fa4d387e272b20c4
Fanny Rostaing <fanny@coffretis.com>
2020-08-12 12:35:29 +0200

    no style, but efficient :)

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true, length: {minimum: 1}
+  # validates :name, presence: true, uniqueness: { scope: :restaurant_id }
+  validates :rating, inclusion: { in: 0..5}, numericality: { only_integer: true }
+end
